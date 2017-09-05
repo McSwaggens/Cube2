74921e328e44d304406ee402ad9075f6f5112bb0
Daniel Jones <McSwaggens@protonmail.com>
2017-09-06 06:52:41 +1000

    Added Texture shader

---
+++ src/shaders/Textured.glsl
@@ -0,0 +1,27 @@
+#version 330 core
+
+#vert
+layout (location = 0) in vec3 vertexPosition_modelspace;
+layout (location = 1) in vec2 vertexUV;
+
+
+uniform mat4 MVP;
+out vec2 UV;
+
+void main ()
+{
+	gl_Position = MVP * vec4(vertexPosition_modelspace.xy, 0, 1);
+	UV = vertexUV;
+}
+
+
+#frag
+out vec3 color;
+in vec2 UV;
+
+uniform sampler2D tex;
+
+void main ()
+{
+	color = texture(tex, UV).rgb;
+}
