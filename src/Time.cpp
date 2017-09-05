95127b54f1361974bd1ec9211d01f0ee28998c0f
Daniel Jones <McSwaggens@protonmail.com>
2017-09-05 13:52:00 +1000

    Added time system

---
+++ src/Time.cpp
@@ -0,0 +1,20 @@
+#include "Time.h"
+
+#include <chrono>
+#include <stdio.h>
+
+float Time::time;
+float Time::delta;
+
+void Time::Update ()
+{
+	float last_time = Time::time;
+	uint64_t actual_time = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+	static uint64_t start_time = actual_time;
+	Time::time = (float)((double)(actual_time - start_time) / 1000.0 / 1000.0);
+	Time::delta = time - last_time;
+	
+	
+	// printf ("Time: %f\n", Time::time);
+	// printf ("Delta: %f\n", Time::delta);
+}
---
+++ src/Time.h
@@ -0,0 +1,16 @@
+// Copyright 2017 Daniel Jones. All Rights Reserved.
+
+/* Time.h
+	Description:
+		Handles all time calculations.
+*/
+
+#pragma once
+
+namespace Time
+{
+	extern float delta;
+	extern float time;
+	
+	void Update ();
+}
