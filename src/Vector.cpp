08d63538c105c13d987752f8dd2ba5ba1168b275
Daniel Jones <McSwaggens@protonmail.com>
2017-09-07 20:32:57 +1000

    Added vector operator overloads

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,4 +1,20 @@
 #include "Vector.h"
+#include "Math.h"
+
+//?
+//? ─── GLOBAL VECTORS ─────────────────────────────────────────────────────────────
+//?
+
+Vector zero (0, 0);
+Vector one (1, 1);
+Vector up (0, 1);
+Vector down (0, -1);
+Vector left (-1, 0);
+Vector right (1, 0);
+
+//?
+//? ─── CONSTRUCTORS ───────────────────────────────────────────────────────────────
+//?
 
 
 Vector::Vector ()
@@ -19,6 +35,12 @@ Vector::Vector (float x, float y)
 	this->y = y;
 }
 
+
+//?
+//? ─── CONVERTING TO GLM DATA TYPES ───────────────────────────────────────────────
+//?
+
+
 glm::vec2 Vector::ToGLM ()
 {
 	return *(glm::vec2*)(void*)this;
@@ -28,3 +50,220 @@ glm::vec3 Vector::ToGLM3 ()
 {
 	return glm::vec3(x, y, 0.0f);
 }
+
+
+//?
+//? ─── COMPARISON OPERATORS ───────────────────────────────────────────────────────
+//?
+
+	bool operator == (const Vector& lhs, const Vector& rhs)
+	{
+		return lhs.x == rhs.x && lhs.y == rhs.y;
+	}
+
+	bool operator != (const Vector& lhs, const Vector& rhs)
+	{
+		return lhs.x != rhs.x && lhs.y != rhs.y;
+	}
+
+	bool operator <  (const Vector& lhs, const Vector& rhs)
+	{
+		return lhs.x < rhs.x && lhs.y < rhs.y;
+	}
+
+	bool operator >  (const Vector& lhs, const Vector& rhs)
+	{
+		return lhs.x > rhs.x && lhs.y > rhs.y;
+	}
+
+	bool operator <= (const Vector& lhs, const Vector& rhs)
+	{
+		return lhs.x <= rhs.x && lhs.y <= rhs.y;
+	}
+
+	bool operator >= (const Vector& lhs, const Vector& rhs)
+	{
+		return lhs.x >= rhs.x && lhs.y >= rhs.y;
+	}
+
+	//?
+	//? ─── FLOAT ──────────────────────────────────────────────────────────────────────
+	//?
+
+	bool operator == (const Vector& lhs, const float& rhs)
+	{
+		return lhs.x == rhs && lhs.y == rhs;
+	}
+
+	bool operator != (const Vector& lhs, const float& rhs)
+	{
+		return lhs.x != rhs && lhs.y != rhs;
+	}
+
+	bool operator <  (const Vector& lhs, const float& rhs)
+	{
+		return lhs.x < rhs && lhs.y < rhs;
+	}
+
+	bool operator >  (const Vector& lhs, const float& rhs)
+	{
+		return lhs.x > rhs && lhs.y > rhs;
+	}
+
+	bool operator <= (const Vector& lhs, const float& rhs)
+	{
+		return lhs.x <= rhs && lhs.y <= rhs;
+	}
+
+	bool operator >= (const Vector& lhs, const float& rhs)
+	{
+		return lhs.x >= rhs && lhs.y >= rhs;
+	}
+	
+	
+	
+	
+	bool operator == (const float& lhs, const Vector& rhs)
+	{
+		return lhs == rhs.x && lhs == rhs.y;
+	}
+
+	bool operator != (const float& lhs, const Vector& rhs)
+	{
+		return lhs != rhs.x && lhs != rhs.y;
+	}
+
+	bool operator <  (const float& lhs, const Vector& rhs)
+	{
+		return lhs < rhs.x && lhs < rhs.y;
+	}
+
+	bool operator >  (const float& lhs, const Vector& rhs)
+	{
+		return lhs > rhs.x && lhs > rhs.y;
+	}
+
+	bool operator <= (const float& lhs, const Vector& rhs)
+	{
+		return lhs <= rhs.x && lhs <= rhs.y;
+	}
+
+	bool operator >= (const float& lhs, const Vector& rhs)
+	{
+		return lhs >= rhs.x && lhs >= rhs.y;
+	}
+
+//?
+//? ─── ASSIGNMENT OPERATORS ───────────────────────────────────────────────────────
+//?
+	
+	Vector& Vector::operator += (const Vector& rhs)
+	{
+		x += rhs.x;
+		y += rhs.y;
+		return *this;
+	}
+	
+	Vector& Vector::operator -= (const Vector& rhs)
+	{
+		x -= rhs.x;
+		y -= rhs.y;
+		return *this;
+	}
+	
+	Vector& Vector::operator *= (const Vector& rhs)
+	{
+		x *= rhs.x;
+		y *= rhs.y;
+		return *this;
+	}
+	
+	Vector& Vector::operator /= (const Vector& rhs)
+	{
+		x /= rhs.x;
+		y /= rhs.y;
+		return *this;
+	}
+	
+	//?
+	//? ─── FLOAT ──────────────────────────────────────────────────────────────────────
+	//?
+	
+	Vector& Vector::operator += (const float& rhs)
+	{
+		x += rhs;
+		y += rhs;
+		return *this;
+	}
+	
+	Vector& Vector::operator -= (const float& rhs)
+	{
+		x -= rhs;
+		y -= rhs;
+		return *this;
+	}
+	
+	Vector& Vector::operator *= (const float& rhs)
+	{
+		x *= rhs;
+		y *= rhs;
+		return *this;
+	}
+	
+	Vector& Vector::operator /= (const float& rhs)
+	{
+		x /= rhs;
+		y /= rhs;
+		return *this;
+	}
+	
+//?
+//? ─── ARITHMETIC ─────────────────────────────────────────────────────────────────
+//?
+	
+
+	Vector operator + (const Vector& lhs, const Vector& rhs)
+	{
+		return Vector (lhs.x + rhs.x, lhs.y + rhs.y);
+	}
+
+	Vector operator - (const Vector& lhs, const Vector& rhs)
+	{
+		return Vector (lhs.x - rhs.x, lhs.y - rhs.y);
+	}
+
+	Vector operator * (const Vector& lhs, const Vector& rhs)
+	{
+		return Vector (lhs.x * rhs.x, lhs.y * rhs.y);
+	}
+
+	Vector operator / (const Vector& lhs, const Vector& rhs)
+	{
+		return Vector (lhs.x / rhs.x, lhs.y / rhs.y);
+	}
+	
+	//?
+	//? ─── FLOAT ──────────────────────────────────────────────────────────────────────
+	//?
+	
+	Vector operator + (const Vector& lhs, const float& rhs)
+	{
+		return Vector (lhs.x + rhs, lhs.y + rhs);
+	}
+
+	Vector operator - (const Vector& lhs, const float& rhs)
+	{
+		return Vector (lhs.x - rhs, lhs.y - rhs);
+	}
+
+	Vector operator * (const Vector& lhs, const float& rhs)
+	{
+		return Vector (lhs.x * rhs, lhs.y * rhs);
+	}
+
+	Vector operator / (const Vector& lhs, const float& rhs)
+	{
+		return Vector (lhs.x / rhs, lhs.y / rhs);
+	}
+	
+//? ────────────────────────────────────────────────────────────────────────────────
--- src/Vector.h
+++ src/Vector.h
@@ -2,12 +2,25 @@
 
 /* Vector.h
 	Description:
-		Vector class
+		Vector Class
 */
 
 #pragma once
 
 #include <glm/glm.hpp>
+
+//?
+//? ─── GLOBAL VECTORS ─────────────────────────────────────────────────────────────
+//?
+
+class Vector;
+
+extern Vector zero;
+extern Vector one;
+extern Vector up;
+extern Vector down;
+extern Vector left;
+extern Vector right;
 
 class Vector
 {
@@ -16,10 +29,96 @@ public:
 	float x;
 	float y;
 	
+	//?
+	//? ─── CONSTRUCTORS ───────────────────────────────────────────────────────────────
+	//?
+	
 	Vector ();
 	Vector (float x);
 	Vector (float x, float y);
 	
+	//?
+	//? ─── CONVERTING TO GLM DATA TYPES ───────────────────────────────────────────────
+	//?
+	
 	glm::vec2 ToGLM ();
 	glm::vec3 ToGLM3 ();
+	
+	//?
+	//? ─── OPERATORS ──────────────────────────────────────────────────────────────────
+	//?
+
+	
+		//?
+		//? ─── COMPARISON OPERATORS ───────────────────────────────────────────────────────
+		//?
+		
+		friend bool operator == (const Vector& lhs, const Vector& rhs);
+		friend bool operator != (const Vector& lhs, const Vector& rhs);
+		friend bool operator <  (const Vector& lhs, const Vector& rhs);
+		friend bool operator >  (const Vector& lhs, const Vector& rhs);
+		friend bool operator <= (const Vector& lhs, const Vector& rhs);
+		friend bool operator >= (const Vector& lhs, const Vector& rhs);
+	
+		//?
+		//? ─── FLOAT ──────────────────────────────────────────────────────────────────────
+		//?
+
+	
+		friend bool operator == (const Vector& lhs, const float& rhs);
+		friend bool operator != (const Vector& lhs, const float& rhs);
+		friend bool operator <  (const Vector& lhs, const float& rhs);
+		friend bool operator >  (const Vector& lhs, const float& rhs);
+		friend bool operator <= (const Vector& lhs, const float& rhs);
+		friend bool operator >= (const Vector& lhs, const float& rhs);
+		
+		friend bool operator == (const float& lhs, const Vector& rhs);
+		friend bool operator != (const float& lhs, const Vector& rhs);
+		friend bool operator <  (const float& lhs, const Vector& rhs);
+		friend bool operator >  (const float& lhs, const Vector& rhs);
+		friend bool operator <= (const float& lhs, const Vector& rhs);
+		friend bool operator >= (const float& lhs, const Vector& rhs);
+	
+		//?
+		//? ─── ASSIGNMENT OPERATORS ───────────────────────────────────────────────────────
+		//?
+		
+		Vector& operator += (const Vector& rhs);
+		Vector& operator -= (const Vector& rhs);
+		Vector& operator *= (const Vector& rhs);
+		Vector& operator /= (const Vector& rhs);
+		
+		//?
+		//? ─── FLOAT ───────────────────────────────────────────────────────
+		//?
+		
+		Vector& operator += (const float& rhs);
+		Vector& operator -= (const float& rhs);
+		Vector& operator *= (const float& rhs);
+		Vector& operator /= (const float& rhs);
+		
+		//?
+		//? ─── ARITHMETIC ──────────────────────────────────────────────────
+		//?
+		
+		friend Vector operator + (const Vector& lhs, const Vector& rhs);
+		friend Vector operator - (const Vector& lhs, const Vector& rhs);
+		friend Vector operator * (const Vector& lhs, const Vector& rhs);
+		friend Vector operator / (const Vector& lhs, const Vector& rhs);
+		friend Vector operator % (const Vector& lhs, const Vector& rhs);
+		
+		//?
+		//? ─── FLOAT ───────────────────────────────────────────────────────
+		//?
+		
+		friend Vector operator + (const Vector& lhs, const float& rhs);
+		friend Vector operator - (const Vector& lhs, const float& rhs);
+		friend Vector operator * (const Vector& lhs, const float& rhs);
+		friend Vector operator / (const Vector& lhs, const float& rhs);
+		friend Vector operator % (const Vector& lhs, const float& rhs);
+		
+		
+		
+	//? ────────────────────────────────────────────────────────────────────────────────
+
 };
