79c1c100c580bd962377a1e85795c39477a95ce9
Daniel Jones <McSwaggens@protonmail.com>
2017-09-08 12:17:42 +1000

    More math functions

--- src/Math.cpp
+++ src/Math.cpp
@@ -1,7 +1,25 @@
 #include "Math.h"
+#include <cmath>
+
+//?
+//? ─── CLAMP ──────────────────────────────────────────────────────────────────────
+//?
 
 
 float Clamp (float v, float min, float max)
 {
 	return v < min ? min : v > max ? max : v;
 }
+
+Vector Clamp (Vector v, Vector min, Vector max)
+{
+	return v < min ? min : v > max ? max : v;
+}
+
+Vector Clamp (Vector v, float min, float max)
+{
+	return v < min ? min : v > max ? max : v;
+}
+
+
+//? ────────────────────────────────────────────────────────────────────────────────
--- src/Math.h
+++ src/Math.h
@@ -7,4 +7,59 @@
 
 #pragma once
 
+#include "Vector.h"
+#include <cmath>
+
+//?
+//? ─── CLAMP ──────────────────────────────────────────────────────────────────────
+//?
+
+
 float Clamp (float v, float min, float max);
+Vector Clamp (Vector v, Vector min, Vector max);
+Vector Clamp (Vector v, float min, float max);
+
+
+//?
+//? ─── SIN ────────────────────────────────────────────────────────────────────────
+//?
+
+
+inline float Sin (float v)
+{
+	return (sin(v));
+}
+
+inline Vector Sin (Vector v)
+{
+	return Vector (sin(v.x), sin(v.y));
+}
+
+
+//?
+//? ─── COS ────────────────────────────────────────────────────────────────────────
+//?
+
+
+inline float Cos (float v)
+{
+	return (cos(v));
+}
+
+inline Vector Cos (Vector v)
+{
+	return Vector (cos(v.x), cos(v.y));
+}
+
+
+//?
+//? ─── DOT ────────────────────────────────────────────────────────────────────────
+//?
+
+
+inline float Dot (Vector a, Vector b)
+{
+	a.x * b.x + a.y * b.y;
+}
+
+//? ────────────────────────────────────────────────────────────────────────────────
