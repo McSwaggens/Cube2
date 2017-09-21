99d0bef0a81d034ce2c268a1b3e262d60ef7c777
Daniel Jones <McSwaggens@protonmail.com>
2017-09-22 01:01:43 +1000

    Implemented DrawQuad to Graphics system

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -3,16 +3,55 @@
 #include "OpenGL.h"
 #include "Defaults.h"
 
+//?
+//? ─── VARIABLES ──────────────────────────────────────────────────────────────────
+//?
+
+	//?
+	//? ─── LINE ───────────────────────────────────────────────────────────────────────
+	//?
+
+
+	GLuint line_uv_buffer_id;
+	const GLfloat uv_buffer_1d_data[] =
+	{
+		0.0f, 1.0f
+	};
+
+	//?
+	//? ─── QUAD ───────────────────────────────────────────────────────────────────────
+	//?
+	
+	GLuint uv_buffer_id;
+	const GLfloat vertex_buffer_data[] =
+	{
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		1.0f,  1.0f, 0.0f,
+		
+		1.0f,  1.0f, 0.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, -1.0f, 0.0f,
+	};
+	
+	GLuint vertex_buffer_id;
+	const GLfloat uv_buffer_data[] =
+	{
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		1.0f, 1.0f,
+		
+		1.0f, 1.0f,
+		0.0f, 1.0f,
+		-0.0f, 0.0f,
+	};
+	
+//? ────────────────────────────────────────────────────────────────────────────────
+
 
 //?
 //? ─── GRAPHICS ───────────────────────────────────────────────────────────────────
 //?
-
-GLuint line_uv_buffer_id;
-const GLfloat uv_buffer_1d_data[] =
-{
-	0.0f, 1.0f
-};
 
 void Graphics::InitGraphics ()
 {
@@ -26,13 +65,81 @@ void Graphics::InitGraphics ()
 	
 	
 	//? ────────────────────────────────────────────────────────────────────────────────
-
+	
+	
+	glGenBuffers (1, &uv_buffer_id);
+	glBindBuffer (GL_ARRAY_BUFFER, uv_buffer_id);
+	glBufferData (GL_ARRAY_BUFFER, sizeof(uv_buffer_data), uv_buffer_data, GL_STATIC_DRAW);
+	
+	
+	
+	glGenBuffers (1, &vertex_buffer_id);
+	glBindBuffer (GL_ARRAY_BUFFER, vertex_buffer_id);
+	glBufferData (GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+	
 }
+
+//?
+//? ─── QUAD ───────────────────────────────────────────────────────────────────────
+//?
 
 void Graphics::DrawQuad (Camera* camera, Transform* transform, Material* material)
 {
+	//?
+	//? ─── ENABLE OUR BUFFERS ─────────────────────────────────────────────────────────
+	//?
+
+	glEnableVertexAttribArray (0);
+	glBindBuffer (GL_ARRAY_BUFFER, vertex_buffer_id);
+	glVertexAttribPointer
+	(
+		0,
+		3,
+		GL_FLOAT,
+		GL_FALSE,
+		0,
+		(void*)0
+	);
 	
+	glEnableVertexAttribArray (1);
+	glBindBuffer (GL_ARRAY_BUFFER, uv_buffer_id);
+	glVertexAttribPointer
+	(
+		1,
+		2,
+		GL_FLOAT,
+		GL_FALSE,
+		0,
+		(void*)0
+	);
+	
+	//?
+	//? ─── DRAW OUR QUAD ──────────────────────────────────────────────────────────────
+	//?
+
+	
+	mat4 mvp = camera->GenerateMVPMatrix (*transform);
+	material->Enable (mvp);
+	
+	glDrawArrays (GL_TRIANGLES, 0, 6);
+	
+	material->Disable ();
+	
+	//?
+	//? ─── DISABLE OUR BUFFERS ────────────────────────────────────────────────────────
+	//?
+	
+	glDisableVertexAttribArray (0);
+	glDisableVertexAttribArray (1);
+	
+	
+	//? ────────────────────────────────────────────────────────────────────────────────
 }
+
+//?
+//? ─── LINE ───────────────────────────────────────────────────────────────────────
+//?
+
 
 void Graphics::DrawLineRaw (Vector a, Vector b, Color color)
 {
