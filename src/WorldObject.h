335d594eaa09648c36851e05711984d00634a646
Daniel Jones <McSwaggens@protonmail.com>
2017-09-24 13:17:20 +1000

    WorldObject tests

--- src/WorldObject.h
+++ src/WorldObject.h
@@ -2,18 +2,29 @@
 
 /* WorldObject.h
 	Description:
-		
+		This is currently being used as a test
 */
 
 #pragma once
 
+#include "Renderer.h"
 #include "Entity.h"
 #include "Transform.h"
+#include "Material.h"
+#include "Defaults.h"
 
+//?
+//? ─── WORLD OBJECT ───────────────────────────────────────────────────────────────
+//?
 
 class WorldObject : public Entity
 {
+public:
 	Transform transform;
+	BlendMaterial material = BlendMaterial(blend_shader);
+	Renderer renderer = Renderer(&transform, &material, RENDERSHAPE_SQUARE);
 	
 	void Start ();
 };
+
+//? ────────────────────────────────────────────────────────────────────────────────
