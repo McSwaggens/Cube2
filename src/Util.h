ae7a5dc5d5216a7d40949b56e83a36e88300b31d
Daniel Jones <McSwaggens@protonmail.com>
2017-09-25 10:04:59 +1000

    Added Util

---
+++ src/Util.h
@@ -0,0 +1,23 @@
+// Copyright 2017 Daniel Jones. All Rights Reserved.
+
+/* Util.h
+	Description:
+		Utility methods
+*/
+
+#pragma once
+
+#include <vector>
+
+namespace Util
+{
+	template<typename T>
+	void RemoveVectorFromVector (std::vector<T>& vresult, std::vector<T>& vremove)
+	{
+		for (int i = 0; i < vremove.size(); i++)
+		{
+			T& t = vremove[i];
+			vresult.erase (std::remove(vresult.begin(), vresult.end(), t), vresult.end());
+		}
+	}
+}
