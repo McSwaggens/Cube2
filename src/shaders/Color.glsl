12562cfcbbf5ec3acb03fa346215efc205c7fa74
Daniel Jones <McSwaggens@protonmail.com>
2017-09-27 00:31:31 +1000

    Fixed Color for OSX

--- src/shaders/Color.glsl
+++ src/shaders/Color.glsl
@@ -6,18 +6,15 @@ layout (location = 1) in vec2 vertexUV;
 
 
 uniform mat4 MVP;
-out vec2 UV;
 
 void main ()
 {
 	gl_Position = vec4(vertexPosition_modelspace.xy, 0, 1);
-	UV = vertexUV;
 }
 
 
 #frag
 out vec4 color;
-in vec2 UV;
 
 uniform vec4 in_color;
 
