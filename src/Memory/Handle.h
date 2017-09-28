ef7f40e003a6477c9aa094cbd3270d66b5bb6adb
Daniel Jones <McSwaggens@protonmail.com>
2017-09-29 02:44:13 +1000

    Fixed Handle equals operator code & made it safer.

--- src/Memory/Handle.h
+++ src/Memory/Handle.h
@@ -7,7 +7,7 @@ public:
 	
 	Handle ()
 	{
-		node = null_node;
+		node = (Node*)null_node;
 	}
 	
 	Handle (Node* node)
@@ -34,18 +34,48 @@ public:
 		this->node->Decrease ();
 	}
 	
+	void Assign (Handle<T>& rval)
+	{
+		// Increase the count on the rval first,
+		// if we (for instance) assign the Handle to itself,
+		// We want to increase the node by 1 and THEN decrease it,
+		// otherwise, it will decrease and delete itself
+		// before we can increase it again.
+		
+		
+		// Increase the rval node
+		if (rval.node)
+		{
+			rval.node->Increase ();
+		}
+		
+		
+		// Decrease the current node
+		if (node)
+		{
+			node->Decrease ();
+		}
+		
+		
+		// Assign the node
+		node = rval.node;
+	}
+	
 	Handle<T>& operator = (Handle<T>& rval)
 	{
-		node->Decrease ();
-		node = rval.node;
-		node->Increase ();
+		// Assign the rval
+		Assign (rval);
 		
+		// Return our self
 		return *this;
 	}
 	
-	Handle<T> operator = (Handle<T> rval)
+	Handle<T>& operator = (const Handle<T>& rval)
 	{
-		(*this) = rval;
+		// Assign the rval
+		Assign ((Handle<T>&)rval);
+		
+		// Return our self
 		return *this;
 	}
 	
